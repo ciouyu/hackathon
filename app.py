c1745c0b30b4ee199ef6ff3e4ef3f6fad6a0a4f0
Tony Kong <48313081+ciouyu@users.noreply.github.com>
2023-10-03 16:28:28 +0800

    2

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask,requese,render_template, redirect,Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('/figma/iphone_14_1/iphone_14___1.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+
