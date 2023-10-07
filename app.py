18f89e9e4fe1220063fe8b8f2bdabeef6023e5c5
Tony Kong <48313081+ciouyu@users.noreply.github.com>
2023-10-07 12:43:13 +0000

    refresh

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ app = Flask(__name__)
 @app.route('/')
 def mainpage():
     return render_template('spacetravel.html')
+@app.route('/1')
+def info1():
+    return render_template('1.html')
+@app.route('/2')
+def info2():
+    return render_template('2.html')
+@app.route('/3')
+def info3():
+    return render_template('3.html')
+@app.route('/4')
+def info4():
+    return render_template('4.html')
+@app.route('/5')
+def info5():
+    return render_template('5.html')
+@app.route('/6')
+def info6():
+    return render_template('6.html')
+@app.route('/7')
+def info7():
+    return render_template('7.html')
+@app.route('/8')
+def info8():
+    return render_template('8.html')
+
+
 
 if __name__ == '__main__':
     app.debug = True
