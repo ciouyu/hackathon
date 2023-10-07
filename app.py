9f0f90c60b1d1e07946f564078f525633c0eb612
Tony Kong <48313081+ciouyu@users.noreply.github.com>
2023-10-07 22:28:33 +0000

    0.0

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ def au():
 @app.route('/contactus')
 def cu():
     return render_template('contactus.html')
-
+@app.route('/craft')
+def craft():
+    return render_template('craft.html')
 @app.route('/1')
 def info1():
     return render_template('1.html')
